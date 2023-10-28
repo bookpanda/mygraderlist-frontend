368ff0fb79f6e23f13bc7b64eff815b95f8457d3
Idhibhat Pankam <marginpankam@gmail.com>
2023-10-28 21:37:27 +0700

    move styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import '@styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,11 +13,7 @@ interface RootLayoutProps {
     children: React.ReactNode;
 }
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
             <body className={inter.className}>{children}</body>
