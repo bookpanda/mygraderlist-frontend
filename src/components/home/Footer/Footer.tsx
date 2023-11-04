0e4e88f6dfc8610f1279faebfbed5c26ae7a3c8a
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-04 22:58:02 +0700

    feat: footer -> currentProblem

--- src/components/home/Footer/Footer.tsx
+++ src/components/home/Footer/Footer.tsx
@@ -5,18 +5,18 @@ import { useDataContext } from '@/context/DataContext';
 import Image from 'next/image';
 
 export const Footer = () => {
-    const { footer } = useDataContext();
+    const { currentProblem } = useDataContext();
     return (
         <div className="absolute bottom-0 left-0 z-10 flex w-full bg-gray-800 p-4">
-            {footer && (
+            {currentProblem && (
                 <Image
                     src={
-                        footer.group === ''
+                        currentProblem.group === ''
                             ? require(
-                                  `@images/courses/${footer.course}/icon.webp`
+                                  `@images/courses/${currentProblem.course}/icon.webp`
                               )
                             : require(
-                                  `@images/courses/${footer.course}/${footer.group}/${footer.code}.webp`
+                                  `@images/courses/${currentProblem.course}/${currentProblem.group}/${currentProblem.code}.webp`
                               )
                     }
                     width={60}
@@ -28,10 +28,10 @@ export const Footer = () => {
             )}
             <div className="ml-4 py-2">
                 <Text variant="p1" className="text-white">
-                    {footer?.name}
+                    {currentProblem?.name}
                 </Text>
                 <Text variant="p3" className="text-gray-text">
-                    {footer?.code}
+                    {currentProblem?.code}
                 </Text>
             </div>
         </div>
