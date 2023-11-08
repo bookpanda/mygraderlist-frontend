9ced874ed718a67189ff36d1a7d38c0818b25e06
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-08 22:07:03 +0700

    feat: footer heart

--- src/components/home/Footer/Footer.tsx
+++ src/components/home/Footer/Footer.tsx
@@ -3,13 +3,14 @@
 import { Text } from '@/components/custom';
 import { useDataContext } from '@/context/DataContext';
 import { FooterImage } from './FooterImage';
+import { LikeButton } from '../LikeButton/LikeButton';
 
 export const Footer = () => {
     const { currentProblem } = useDataContext();
     return (
-        <div className="absolute bottom-0 left-0 z-10 flex w-full bg-gray-800 p-4">
+        <div className="absolute bottom-0 left-0 z-10 flex w-full items-center space-x-4 bg-gray-800 p-4">
             {currentProblem && <FooterImage problem={currentProblem} />}
-            <div className="ml-4 py-2">
+            <div className="py-2">
                 <Text variant="p1" className="text-white">
                     {currentProblem?.name}
                 </Text>
@@ -17,6 +18,13 @@ export const Footer = () => {
                     {currentProblem?.code}
                 </Text>
             </div>
+            {currentProblem && (
+                <LikeButton
+                    heart={currentProblem.heart}
+                    id={currentProblem.id}
+                    show
+                />
+            )}
         </div>
     );
 };
