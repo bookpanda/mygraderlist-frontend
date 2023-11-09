4732348c809b19b6db449d857fef3707271c7b72
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-09 17:01:41 +0700

    fix: move emoji modal out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,11 @@ export default function Home() {
     return (
         <>
             <EditModal />
+            <EmojiModal />
             <div className="hidden h-screen w-full px-4 pt-8 xl:block">
                 <div className="flex h-[90%] space-x-2">
                     <SideBar />
                     <MainList />
-                    <EmojiModal />
                     <ProblemModal />
                 </div>
                 <Footer />
