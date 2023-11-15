7dbf4edfe7fdc80bf07bff271d4c28daa0347dc0
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-15 21:18:26 +0700

    fix: typo

--- src/api/like.ts
+++ src/api/like.ts
@@ -6,7 +6,7 @@ import { ILikes } from '@/types/like';
 export const getUserLikes = async (): Promise<ILikes | null> => {
     let res: AxiosResponse;
     try {
-        res = await apiClient.get<ILikes>('/emoji/mylikes/');
+        res = await apiClient.get<ILikes>('/like/mylikes/');
     } catch (err) {
         return null;
     }
