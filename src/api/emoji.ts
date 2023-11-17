4d7e9839630d5743b1876ebb31aaca6822024e58
Idhibhat Pankam <marginpankam@gmail.com>
2023-11-17 18:13:39 +0700

    feat: emoji queries

--- src/api/emoji.ts
+++ src/api/emoji.ts
@@ -1,7 +1,7 @@
 import { AxiosResponse } from 'axios';
 
 import { apiClient } from './axios';
-import { IEmojis } from '@/types/emoji';
+import { EmojiDto, IEmoji, IEmojis } from '@/types/emoji';
 
 export const getAllEmojis = async (): Promise<IEmojis | null> => {
     let res: AxiosResponse;
@@ -23,4 +23,46 @@ export const getUserEmojis = async (): Promise<IEmojis | null> => {
     }
 
     return res.data;
+};
+
+export const createEmoji = async (emoji: EmojiDto): Promise<IEmoji> => {
+    let res: AxiosResponse;
+    try {
+        res = await apiClient.post<IEmoji>('/emoji/', {
+            emoji: emoji.emoji,
+            problem_id: emoji.problemId,
+            user_id: emoji.userId,
+        });
+    } catch (err: any) {
+        const message = err.response?.data.message;
+        if (message) {
+            throw new Error(message);
+        }
+        throw new Error('No Response from Server');
+    }
+
+    if (res.status !== 201) {
+        throw new Error(res.data.message);
+    }
+
+    return res.data;
+};
+
+export const deleteEmoji = async (id: string): Promise<boolean> => {
+    let res: AxiosResponse;
+    try {
+        res = await apiClient.delete<boolean>(`/emoji/${id}`);
+    } catch (err: any) {
+        const message = err.response?.data.message;
+        if (message) {
+            throw new Error(message);
+        }
+        throw new Error('No Response from Server');
+    }
+
+    if (res.status !== 200) {
+        throw new Error(res.data.message);
+    }
+
+    return true;
 };
--- src/types/emoji.ts
+++ src/types/emoji.ts
@@ -1,6 +1,14 @@
-export type IEmojis = {
+export type IEmoji = {
     id: string;
     emoji: string;
     problemId: string;
     userId: string;
-}[];
+};
+
+export type IEmojis = IEmoji[];
+
+export type EmojiDto = {
+    emoji: string;
+    problemId: string;
+    userId: string;
+};
